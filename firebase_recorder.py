e2cf22c998d8b433fb84832903d5c93faf1e0654
kantentenka <yutakimata@gmail.com>
2022-03-28 14:16:22 +0900

    一定フレームごとにzip化

--- firebase_recorder.py
+++ firebase_recorder.py
@@ -6,12 +6,14 @@ from helpers import colorize
 from pyk4a import Config, PyK4A, connected_device_count, PyK4ARecord
 
 import threading
-
+import time
 import datetime
 
+import zipfile
 
+import subprocess
+from subprocess import Popen
 
-file_name = "test"
 def cnt_device():
     cnt = connected_device_count()
     
@@ -21,10 +23,10 @@ def cnt_device():
 is_recording = False
 
 class Recorder():
-    def __init__(self):
+    def __init__(self,remove=True):
         tdatetime = datetime.datetime.now()
         tstr = tdatetime.strftime('%Y%m%d_%H%M%S')
-        self.file_name = tstr
+        self.prepared_name = tstr
         self.is_recording = False
         self.cnt = cnt_device()
         self.k4a = []
@@ -33,7 +35,10 @@ class Recorder():
             color_resolution=pyk4a.ColorResolution.RES_720P,
             depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
             synchronized_images_only=True,
+            camera_fps=pyk4a.FPS.FPS_15,
         )
+
+        self.remove = remove
     def init_device(self):
         self.is_recording = True
         for i in range(self.cnt):
@@ -52,8 +57,9 @@ class Recorder():
             assert self.k4a[i].whitebalance == 4510
     def is_k4a(self):
         return len(self.k4a)>0
-    def start(self,file_name):
-        self.file_name = file_name
+    def start(self,file_name=""):
+        if file_name == "":
+            file_name = self.prepared_name
         self.is_recording = True
         if not self.cnt:
             print("No devices available")
@@ -62,50 +68,81 @@ class Recorder():
         if not(self.is_k4a()):
             print("device init")
             self.init_device()
-        self.record = []
+        #self.record = []
         print(len(self.k4a)>0)
         for i in range(self.cnt):
-            self.record.append(PyK4ARecord(
+            rec = PyK4ARecord(
                 device=self.k4a[i],
                 config=self.config,
-                path=f"./record/{file_name}_{i}.mkv")
-                          )
-            self.record[i].create()
+                path=f"./record/{file_name}_{i}_0.mkv")
+                          
+            rec.create()  
 
-        th_list = []
-        for i in range(self.cnt):
-            tmp = threading.Thread(target=lambda : self.recorder(self.k4a[i],self.record[i],i))#,daemon=True)
-            tmp.start()
-            th_list.append(tmp)
+    
+            thread = threading.Thread(target=lambda : self.recorder(file_name,self.k4a[i],rec,i,0))#,daemon=True)
+            thread.start()
+            print(f"test{i}") 
 
 
-    def recorder(self,k4a,record,num):
+    def recorder(self,file_name,k4a,record,device_num,file_num):
         cnt = 0
-        while self.is_recording:
-            
+        print(f"device {device_num} file {file_num} record start")
+        #while self.is_recording:
+        #for i in range(150):
+        for i in range(15*3):
             capture = k4a.get_capture()
             record.write_capture(capture)
-            if cnt != record.captures_count:
-                print(f"device {num} captured,frame {record.captures_count}")
-                cnt = record.captures_count
-        print(f"device {num} finish")
+            if not(self.is_recording):
+                break
+        #print(f"device {device_num} file {file_num} finish")
+
+        if self.is_recording:
+            next_rec = PyK4ARecord(
+                    device=k4a,
+                    config=self.config,
+                    path=f"./record/{file_name}_{device_num}_{file_num+1}.mkv")
+            next_rec.create()
+            
+            thread = threading.Thread(target=lambda : self.recorder(file_name,k4a,next_rec,device_num,file_num+1))#,daemon=True)
+            thread.start()
+        
+
+        #print(f"device {device_num} file {file_num} closed")
+        fullname = f"./{file_name}_{device_num}_{file_num}.mkv"
+        print(f"{record.captures_count} frames written on {file_name}_{device_num}_{file_num}")
         record.flush()
         record.close()
-        print(f"device {num} closed")
-        print(f"{record.captures_count} frames written on {self.file_name}_{num}.mkv.")
+        del record
+        #subprocess.run(["powershell","compress-archive",f"./record/{fullname}",f"D:/NICT/{fullname}.zip" ])
+        Popen(["powershell","compress-archive",f"./record/{fullname}",f"D:/NICT/{fullname}.zip" ],shell = True)
+        #self.zip(f"{file_name}_{device_num}_{file_num}")
 
+        
     def stop(self):
         self.is_recording = False
 
- 
-      
-if __name__ == "__main__":
-    recorder = firebase_recorder.Recorder()
+    def zip(self,file_name):
+        self.start = time.time()
+        print(f"{file_name} is zipping")
+        with zipfile.ZipFile(f'D:/NICT/{file_name}.zip', 'w',
+                     compression=zipfile.ZIP_DEFLATED,
+                     compresslevel=9) as zf:
+            zf.write(f'./record/{file_name}.mkv',f'{file_name}.mkv')
+        print(f"{file_name} was zipped in {time.time() -self.start} s")
+        if self.remove:
+            pass
 
+
+
+if __name__ == "__main__":
+    recorder = Recorder(remove = False)
+    import sys, signal
+    def signal_handler(signal, frame):
+        recorder.stop()
+        print("\nprogram exiting gracefully")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    recorder.start()
     while True:
-        try:
-            print("recording")
-        except KeyboardInterrupt:
-            print("CTRL-C pressed. Exiting.")
-            recorder.stop()
-            break
+        pass
